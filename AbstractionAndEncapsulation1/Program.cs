75dfd3945458d2e613e7256ec28a079c352d12c5
OliverMengich <kemeioliver2000@gmail.com>
2019-10-12 06:52:37 +0300

    Abstraction in C#

---
+++ AbstractionAndEncapsulation1/Program.cs
@@ -0,0 +1,33 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionAndEncapsulation1
+{
+      
+    class Program:MyClass
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+     
+            Program program = new Program();
+            program.number1 = 2;
+
+            Console.Read();
+
+        }
+    }
+    class MyClass
+    {
+        protected int number1; // Gets Inherited by the child Class
+        public string MyString()
+        {
+           int number;
+
+            return " ";
+        }
+    }
+}
